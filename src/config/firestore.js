611a39d9f7d6c3c8dc9379d5f3aa955ca88d4d81
PrasannaDommalapati <prasanna.dommalapati@zonal.co.uk>
2024-02-29 16:30:00 +0000

    temp secrets

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -19,15 +19,23 @@ import {
 } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIRE_APIKEY,
-  authDomain: process.env.REACT_APP_FIRE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIRE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIRE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIRE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIRE_APP_ID
+  apiKey: "AIzaSyAMmY6TspJ4eTpXLHrZ90yvOtJ94_HOI-E",
+  authDomain: "session-manager-c7f30.firebaseapp.com",
+  projectId: "session-manager-c7f30",
+  storageBucket: "session-manager-c7f30.appspot.com",
+  messagingSenderId: "415364400030",
+  appId: "1:415364400030:web:132b52b4097b271c030c8f"
 };
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIRE_APIKEY,
+//   authDomain: process.env.REACT_APP_FIRE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIRE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIRE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIRE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIRE_APP_ID
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
